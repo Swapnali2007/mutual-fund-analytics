f66218effb8ee367f4f00ad4711d34d98bfb7740
Swapnali2007 <swapnalianilkamble@gmail.com>
2026-06-22 01:22:40 +0530

    Day 1: Data ingestion complete

--- live_nav_fetch.py
+++ live_nav_fetch.py
@@ -1,5 +1,31 @@
 import requests
-url = " https://api.mfapi.in/mf/125497"
-response = requests.get(url)
-data = response.json()
-print(data)
+import pandas as pd
+import os
+
+# folder create if not exists
+os.makedirs("data/raw", exist_ok=True)
+
+schemes = {
+    "SBI_Bluechip": 119551,
+    "ICICI_Bluechip": 120503,
+    "nippon_Large_Cap": 118632,
+    "Axis_Bluechip": 119092,
+    "Kotak_Bluechip": 120841
+}
+
+for fund_name, code in schemes.items():
+    url = f"https://api.mfapi.in/mf/{code}"
+
+    response = requests.get(url)
+    data = response.json()
+
+    print("Fetching:", data["meta"]["scheme_name"])
+
+    df = pd.DataFrame(data["data"])
+
+    file_name = f"data/raw/{fund_name}.csv"
+    df.to_csv(file_name, index=False)
+
+    print(file_name, "saved successfully")
+
+print("All 5 NAV files fetched")
