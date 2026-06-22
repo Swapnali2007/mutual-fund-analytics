8695ce3115c4702423c2a3fd56747ab75ffa89dd
Swapnali2007 <swapnalianilkamble@gmail.com>
2026-06-22 15:39:08 +0530

    Day 1: Data ingestion complete

---
+++ data_ingestion.py
@@ -0,0 +1,35 @@
+import pandas as pd
+import os
+
+data_path = "data/raw"
+
+files = [
+    "01_fund_master.csv",
+    "02_nav_history.csv",
+    "03_aum_by_fund_house.csv",
+    "04_monthly_sip_inflows.csv",
+    "05_category_inflows.csv",
+    "06_industry_folio_count.csv",
+    "07_scheme_performance.csv",
+    "08_investor_transactions.csv",
+    "09_portfolio_holdings.csv",
+    "10_benchmark_indices.csv"
+
+]
+
+for file in files:
+    filepath = os.path.join(data_path, file)
+
+    df = pd.read_csv(filepath)
+
+    print("\n" + "="*50)
+    print("File:", file)
+
+    print("Shape:")
+    print(df.shape)
+
+    print("\nData Types:")
+    print(df.dtypes)
+
+    print("\nFirst 5 Rows:")
+    print(df.head())
